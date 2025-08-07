6d925deac010ba716ebdb7c3176d717395040d2a
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-07 15:38:50 +0200

    feat: Refactor to a data-driven matrix filter pattern
    - Abstracted matrix multiplication into a dedicated helper function.
    - Created a generic 'apply_matrix_filter' function.
    - Defined filter matrices (SEPIA, GRAYSCALE_LUMA) as constants.
    - The main program now dynamically applies a filter based on user input.

--- 02_Image_Processing/image_filter.py
+++ 02_Image_Processing/image_filter.py
@@ -1,22 +1,54 @@
 from PIL import Image
 import os
 
+SEPIA_MATRIX = (
+    (0.393, 0.769, 0.189),
+    (0.349, 0.686, 0.168),
+    (0.272, 0.534, 0.131)
+)
+
+
+GRAYSCALE_LUMA_MATRIX = (
+    (0.299, 0.587, 0.114),
+    (0.299, 0.587, 0.114),
+    (0.299, 0.587, 0.114)
+)
+
+
+IDENTITY_MATRIX = (
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1)
+)
+
 INPUT_FOLDER = "input_images"
 OUTPUT_FOLDER = "output_images"
 IMAGE_NAME = "marseille.jpg"
 
 
-def apply_grayscale_filter(original_image):
+def multiply_color_by_matrix(color_tuple, matrix):
+    r, g, b = color_tuple
+
+    new_r = int((r * matrix[0][0]) + (g * matrix[0][1]) + (b * matrix[0][2]))
+    new_g = int((r * matrix[1][0]) + (g * matrix[1][1]) + (b * matrix[1][2]))
+    new_b = int((r * matrix[2][0]) + (g * matrix[2][1]) + (b * matrix[2][2]))
+
+    new_r = max(0,min(255, new_r))
+    new_g = max(0, min(255, new_g))
+    new_b = max(0, min(255, new_b))
+
+    return new_r, new_g, new_b
+
+def apply_matrix_filter(original_image, matrix):
     width, height = original_image.size
     new_img = Image.new("RGB", (width, height))
 
-    print("-> Applying Grayscale: Processing pixels...")
     for x in range(width):
         for y in range(height):
-            r, g, b = original_image.getpixel((x, y))
-            avg = (r + g + b) // 3
-            new_color = (avg, avg, avg)
-            new_img.putpixel((x, y), new_color)
+            old_pixel = original_image.getpixel((x, y))
+
+            new_pixel = multiply_color_by_matrix(old_pixel, matrix)
+            new_img.putpixel((x, y), new_pixel)
 
     return new_img
 
@@ -41,7 +73,11 @@ try:
     filtered_image = None
 
     if user_choice == "GRAYSCALE":
-        filtered_image = apply_grayscale_filter(original_image)
+        filtered_image = apply_matrix_filter(original_image, GRAYSCALE_LUMA_MATRIX)
+    elif user_choice == "SEPIA":
+        print("Applying Sepia filter...")
+        filtered_image = apply_matrix_filter(original_image, SEPIA_MATRIX)
+
     else:
         print(f"Error: Unknown filter: '{user_choice}'.")
 
