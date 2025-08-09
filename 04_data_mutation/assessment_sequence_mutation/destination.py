32dcd7e1adc1c87592e4f7d1172f0f9ca41f8d9a
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-09 12:51:02 +0200

    feat(assessment): Remove item from a list by value

---
+++ 04_data_mutation/assessment_sequence_mutation/destination.py
@@ -0,0 +1,7 @@
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'London', 'Melbourne']
+
+
+dest_to_remove = "London"
+
+trav_dest.remove(dest_to_remove)
+print(trav_dest)
