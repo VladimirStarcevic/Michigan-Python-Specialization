7669b3e7da503b7a51087d77c20bc8b006cecf51
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-09 18:10:10 +0200

    feat(assessment): Transform list of words to past tense

---
+++ 04_data_mutation/accumulator_pattern_list_strings/past_words_list.py
@@ -0,0 +1,6 @@
+wrds = ["end", 'work', "play", "start", "walk", "look", "open", "rain", "learn", "clean"]
+past_words = []
+
+for word in wrds:
+    past_words.append(word + "ed")
+print(past_words)
