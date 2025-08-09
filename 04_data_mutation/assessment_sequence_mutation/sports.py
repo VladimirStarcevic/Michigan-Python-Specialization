a741b3242c47ec8845085ea7f4bcf756135511e7
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-09 12:47:14 +0200

    feat(assessment): Insert item into a specific list position

---
+++ 04_data_mutation/assessment_sequence_mutation/sports.py
@@ -0,0 +1,6 @@
+sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+
+sport = "horseback riding"
+
+sports.insert(2, sport)
+print(sports)
