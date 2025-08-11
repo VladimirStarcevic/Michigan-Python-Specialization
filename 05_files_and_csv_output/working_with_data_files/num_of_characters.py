b143b72b6cd96df484a44f83c48f2f25639fe073
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-11 22:05:09 +0200

    feat(project): Count total characters in a text file
    - Implemented file reading using a 'with open' block.
    - Used the .read() method to get the entire file content.
    - Calculated the total number of characters with len().

---
+++ 05_files_and_csv_output/working_with_data_files/num_of_characters.py
@@ -0,0 +1,22 @@
+import os
+
+data_folder = "data"
+file_name = "school_prompt2.txt"
+
+file_path = os.path.join("..", data_folder, file_name)
+
+try:
+    with open(file_path, "r") as file_object:
+
+        content = file_object.read()
+        num_char = len(content)
+
+
+
+except FileNotFoundError:
+    print(f"ERROR: File {file_name} not found!")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+print(f"The file has {num_char} characters.")
+
