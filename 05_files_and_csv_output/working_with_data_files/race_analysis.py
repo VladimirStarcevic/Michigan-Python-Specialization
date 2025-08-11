844d614d2fb1cacb12df379d6d69149ae0be33a3
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-12 00:48:39 +0200

    feat(project): Create podium CSV from F1 results file
    - Implemented file I/O using nested 'with' statements.
    - Processed the header and first three data lines.
    - Wrote formatted output to a new CSV file.

---
+++ 05_files_and_csv_output/working_with_data_files/race_analysis.py
@@ -0,0 +1,37 @@
+import os
+
+data = "data"
+file_name = "monza_2023_results.txt"
+output_file_name = "monza_podium.csv"
+
+file_path = os.path.join("..", data, file_name)
+output_file_path = os.path.join("..", data, output_file_name)
+
+try:
+    with open(file_path, "r") as f_in:
+        with open(output_file_path, "w") as f_out:
+            header = f_in.readline()
+            column = header.strip().split(",")
+            header_info = column[:3]
+            header_column = ",".join(header_info)
+            f_out.write(header_column + "\n")
+
+            drivers_processed = 0
+            for line in f_in:
+
+                if drivers_processed >= 3:
+                    break
+
+                drivers_processed += 1
+
+                info_line = line.strip().split(",")
+                new_info = info_line[:3]
+
+                driver_info = ",".join(new_info)
+                f_out.write(driver_info + "\n")
+
+except FileNotFoundError:
+    print(f"ERROR: File {file_name} not found!")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
