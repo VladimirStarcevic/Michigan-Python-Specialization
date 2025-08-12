0230256f084c3680871fd1fa62d222dcd569ea48
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-13 01:02:54 +0200

    feat(challenge): Extract the third word from each line

---
+++ 05_files_and_csv_output/assessments/call_me_three.py
@@ -0,0 +1,27 @@
+
+import os
+
+data = "data"
+file_name = "travel_plans.txt"
+file_path = os.path.join("..", data, file_name)
+
+three = []
+
+try:
+
+    with open(file_path, "r") as f_input:
+        for line in f_input:
+
+            word_list = line.strip().split()
+
+            if len(word_list) >= 3:
+                third_word = word_list[2]
+                three.append(third_word)
+
+
+except FileNotFoundError:
+    print(f"ERROR: File {file_name} not found!")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+print(three)
