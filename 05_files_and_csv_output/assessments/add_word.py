e72f4156d7ff2e2c05b2bbdd6fb3fe2c31deba05
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-13 01:22:16 +0200

    feat(challenge): Filter words containing a specific character

---
+++ 05_files_and_csv_output/assessments/add_word.py
@@ -0,0 +1,26 @@
+import os
+
+data = "data"
+file_name = "school_prompt.txt"
+file_path = os.path.join("..", data, file_name)
+
+p_words = []
+
+try:
+
+    with open(file_path, "r") as f_input:
+        for line in f_input:
+
+            word_list = line.strip().split()
+
+            for word in word_list:
+                if "p" in word:
+                  p_words.append(word)
+
+
+except FileNotFoundError:
+    print(f"ERROR: File {file_name} not found!")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+print(p_words)
