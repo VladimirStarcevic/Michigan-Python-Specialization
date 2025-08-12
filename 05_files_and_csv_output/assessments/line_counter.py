651237c94031fcff4deee7c187286d37477133dd
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-13 00:27:58 +0200

    feat(assessment): Count total lines in a file

---
+++ 05_files_and_csv_output/assessments/line_counter.py
@@ -0,0 +1,21 @@
+import os
+
+data = "data"
+file_name = "school_prompt.txt"
+
+file_path = os.path.join("..", data, file_name)
+num_lines = 0
+try:
+
+    with open(file_path, "r") as f_input:
+
+        for line in f_input:
+            num_lines += 1
+
+
+except FileNotFoundError:
+    print(f"ERROR: File {file_name} not found!")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+print(f"Number of lines in file {file_name} is: {num_lines}")
