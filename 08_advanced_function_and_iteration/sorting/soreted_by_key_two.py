52588df7cd74b2837fb5659991e58cc6eea7fa7f
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-28 00:49:09 +0200

    feat(lab-sort): Create a sorted list of dictionary keys

---
+++ 08_advanced_function_and_iteration/sorting/soreted_by_key_two.py
@@ -0,0 +1,8 @@
+
+groceries = {'apples': 5, 'pasta': 3, 'carrots': 12, 'orange juice': 2, 'bananas': 8, 'popcorn': 1, 'salsa': 3, 'cereal': 4, 'coffee': 5, 'granola bars': 15, 'onions': 7, 'rice': 1, 'peanut butter': 2, 'spinach': 9}
+
+
+grocery_keys_sorted = sorted(groceries.keys())
+
+for key in grocery_keys_sorted:
+    print(f"{key} => {groceries[key]}")
