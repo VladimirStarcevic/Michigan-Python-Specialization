d0dd174ff64637bb519e0c65222b016a48b279e4
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-08-28 19:26:35 +0200

    test(assessment-sort): Sort dictionary keys by value in descending order

---
+++ 00_coursera_lab_materials/course_2/assignments/course2_assignment5/assessments/groceries_store.py
@@ -0,0 +1,4 @@
+groceries = {'apples': 5, 'pasta': 3, 'carrots': 12, 'orange juice': 2, 'bananas': 8, 'popcorn': 1, 'salsa': 3, 'cereal': 4, 'coffee': 5, 'granola bars': 15, 'onions': 7, 'rice': 1, 'peanut butter': 2, 'spinach': 9}
+most_needed = sorted(groceries, key= lambda value: groceries[value], reverse=True)
+
+print(most_needed)
