4323c88e3ae01611cef19a4da96868928183aed8
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-09-04 14:36:14 +0200

    feat: Use filter to select items from lists Solved exercises using the filter function. Used a lambda function to define the filtering rule. This created new lists with only the words that matched the condition.

---
+++ 09_nested_data_and_nested _iteration/map_and_filter/w_list.py
@@ -0,0 +1,7 @@
+# Write code to assign to the variable filter_testing all the elements in lst_check that have a “w” in them using filter
+
+lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peaches', 'apples', 'mangos', 'papaya']
+
+filter_testing = list(filter(lambda word: 'w' in word, lst_check))
+
+print(filter_testing)
